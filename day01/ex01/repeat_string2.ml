d0e7675363a14fe93d71de0a81783b9e87961927
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2020-01-19 03:15:05 +0100

    day01: ex01: add recursive terminale

---
+++ day01/ex01/repeat_string2.ml
@@ -0,0 +1,17 @@
+let repeat_string ?str:(s="x") x = 
+        let rec repeat_string_terminale s x acc =
+                if x < 0
+                then "Error"
+                else if x = 0
+                then ""
+                else (repeat_string_terminale s (x - 1) (acc ^ s))
+
+let main () = 
+        print_endline (repeat_string (-1));
+        print_endline (repeat_string 0);
+        print_endline (repeat_string ~str:"Toto" 1);
+        print_endline (repeat_string 2);
+        print_endline (repeat_string ~str:"a" 5);
+        print_endline (repeat_string ~str:"what" 3)
+
+let () = main ()
