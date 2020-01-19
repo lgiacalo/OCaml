003768e9df0e1fcb2d5ba2192586d963719ba645
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2020-01-19 03:02:49 +0100

    day01: ex00: ok

---
+++ day01/ex00/repeat_x.ml
@@ -0,0 +1,17 @@
+
+let rec repeat_x x = 
+        if x < 0
+        then "Error"
+        else if x = 0
+        then ""
+        else ("x" ^ (repeat_x (x - 1)))
+
+
+let main () = 
+        print_endline (repeat_x (-1));
+        print_endline (repeat_x 0);
+        print_endline (repeat_x 1);
+        print_endline (repeat_x 2);
+        print_endline (repeat_x 5)
+
+let () = main ()
