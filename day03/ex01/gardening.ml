eddd3a3fed0d37c990d5efd8eee62a2c3ea1e4cd
Lena GIACALONE <lgiacalo@student.42.fr>
2020-01-22 18:58:11 +0100

    Day03: ex01: en cours

---
+++ day03/ex01/gardening.ml
@@ -0,0 +1,24 @@
+(*
+
+La racine d'un arbre binaire est le nœud supérieur. 
+Une feuille d'un arbre binaire est un nœud sans sous-arbre. 
+La taille d'un arbre binaire est le nombre de nœuds définis. 
+La hauteur d'un arbre binaire est le nombre de relations sur le chemin descendant le plus long entre la racine et une feuille
+
+Écrivez une taille de fonction qui prend un arbre comme paramètre et renvoie sa taille. 
+La fonction doit être saisie comme suit:
+       val size: 'a tree -> int
+
+Écrivez une hauteur de fonction qui prend un arbre comme paramètre et renvoie sa hauteur.
+La fonction doit être saisie comme suit:
+        val height: 'a tree -> int
+
+Écrivez une fonction draw_tree qui prend un arbre comme paramètre et le dessine en utilisant le module graphique. 
+Nous supposons que la taille de la fenêtre est suffisante pour la dessiner, vous n'avez donc pas à la gérer. 
+Nous ne considérerons que les arbres de type arbre de chaînes pour cette fonction, vous pouvez donc dessiner les valeurs sans problème de conversion.
+La fonction doit être saisie comme suit:
+        val draw_tree: string tree -> unit
+
+
+
+ *)
