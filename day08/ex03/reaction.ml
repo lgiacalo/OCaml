da7626f1a87562c4c2818bc13223a07f7b3c85c6
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2020-01-30 23:58:17 +0100

    day08: ex03: ok

---
+++ day08/ex03/reaction.ml
@@ -0,0 +1,12 @@
+
+
+class virtual reaction ((l_in:Molecule.molecule list), (l_out:Molecule.molecule list)) = 
+object
+
+	method virtual get_start 	: (Molecule.molecule * int) list
+	method virtual get_result	: (Molecule.molecule * int) list
+	
+	method virtual balance		: reaction
+	method virtual is_balanced	: bool
+
+end
