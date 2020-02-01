460c7cae1eeaed1bb222260641d41acdd778cc59
lgiacalo <lgiacalo@mbp-de-lgiacalo.home>
2020-02-01 10:06:45 +0100

    day09: ex02: ok

--- day09/ex02/monoid.ml
+++ day09/ex02/monoid.ml
@@ -25,7 +25,7 @@ end
 
 module FLOAT = 
 struct
-	type element = int
+	type element = float
 
 	let zero1 = 0.
 	let zero2 = 1.
@@ -64,40 +64,30 @@ module Calc =
 
 		let fact x = if (x = M.zero1) then M.zero2 else
 					let rec loop n acc = match n with
-						| nb when (nb = M.zero1) -> acc
-						| nb -> loop (M.sub nb M.zero2) (M.mul acc M.zero2)
+						| nb when (nb = M.zero2) -> acc
+						| nb -> loop (M.sub nb M.zero2) (M.mul acc (nb))
 					in loop x M.zero2
 	end
 
-
-
-module Calc_int = Calc(INT)
-
-(* module Calc_float = Calc(FLOAT) *)
+module Calc_int 	= Calc(INT)
+module Calc_float 	= Calc(FLOAT)
 
 
 let () =
 	print_endline (string_of_int (Calc_int.power 3 3));
-	print_endline (string_of_int (Calc_int.fact 3 ));
+	print_endline (string_of_float (Calc_float.power 3.0 3));
+
+	print_endline (string_of_int (Calc_int.fact 4));
+	print_endline (string_of_float (Calc_float.fact 4.));
+
 	print_endline (string_of_int (Calc_int.mul (Calc_int.add 20 1) 2));
+	print_endline (string_of_float (Calc_float.mul (Calc_float.add 20.0 1.0) 2.0));
 
+	print_endline (string_of_int (Calc_int.div (Calc_int.sub 23 1) 2));
+	print_endline (string_of_float (Calc_float.div (Calc_float.sub 23. 1.) 2.));
 
-
-
-(* 		let power x y = let rec loop n acc = match n with
-						| nb when (nb = 0) -> (acc)
-						| nb -> loop (n - 1) (acc * x)
-					in loop y 1 *)
-
-(* 	print_endline ("3^0 = " ^ (string_of_int (power 3 0)));
-	print_endline ("3^1 = " ^ (string_of_int (power 3 1)));
-
-	print_endline ("2^3 = " ^ (string_of_int (power 2 3)));
-	print_endline ("2^4 = " ^ (string_of_int (power 2 4)));
-	print_endline ("2^5 = " ^ (string_of_int (power 2 5)));
-	print_endline ("2^6 = " ^ (string_of_int (power 2 6))); *)
-
+(* TODO: fact de -1 ?? *)
+	(* print_endline (string_of_int (Calc_int.fact (-1))); *)
 
 	()
-
 
